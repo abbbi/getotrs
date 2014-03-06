21ba34671a53bdf6fadee8af12732efc7c1586b2
Michael Ablassmeier <ma@sep.de>
2014-03-06 12:29:16 +0100

    some error handing

--- getotrs.py
+++ getotrs.py
@@ -6,11 +6,11 @@ import requests
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--url", help="Base URL to otrs (http://host/)", type=str, required=1)
-parser.add_argument("--ticket", help="Ticket URL to download (otrs/index.pl?Action=AgentTicketZoom;TicketID=7496)", type=str, required=1)
-parser.add_argument("--user", help="OTRS Username", type=str, required=1)
-parser.add_argument("--pw", help="OTRS Password", type=str, required=1)
-parser.add_argument("--folder", help="Folder to download stuff (default full subject ticket id)", type=str, required=0)
+parser.add_argument('--url', help='Base URL to otrs (http://host/)', type=str, required=1)
+parser.add_argument('--ticket', help='Ticket URL to download (otrs/index.pl?Action=AgentTicketZoom;TicketID=7496)', type=str, required=1)
+parser.add_argument('--user', help='OTRS Username', type=str, required=1)
+parser.add_argument('--pw', help='OTRS Password', type=str, required=1)
+parser.add_argument('--folder', help='Folder to download stuff (default full subject ticket id)', type=str, required=0)
 args = parser.parse_args()
 
 base_url = args.url
@@ -20,13 +20,26 @@ password = args.pw
 
 browser=mechanize.Browser()
 
-browser.open(base_url+otrs_path)
+try:
+    browser.open(base_url+otrs_path)
+except mechanize.URLError, e:
+    print 'ERROR opening site:' + str(e)
+    exit(1)
+except mechanize.HTTPError, e:
+    print 'ERROR downloading:' + str(e)
+    exit(1)
+
+
 browser.select_form(name='login')
 browser['User'] = username
 browser['Password'] = password
 browser.submit()
 
 data = BeautifulSoup(browser.response().read())
+
+if 'Insufficient Rights' in  data.get_text():
+    print 'Unable to open ticket information: wrong url?'
+    exit(1)
 
 if 'Login failed!' in data.get_text():
     print 'Login failed! Your username or password was entered incorrectly.'
@@ -37,12 +50,12 @@ if args.folder:
 else:
     target_folder =  data.title.string.split(' ')[0]
 
-print "Target Folder:" + target_folder
+print 'Target Folder:' + target_folder
 
 attachments=[]
 for a in data.find_all('a', href=True):
     if 'AgentTicketAttachment' in a['href']:
-        print "Found Attachment:", a['href']
+        print 'Found Attachment:', a['href']
         if a['href'] not in attachments:
             attachments.append(a['href'])
 
