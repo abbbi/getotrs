20ec8879de46e3cc07c28a83a84add1fc65bb7f0
Michael Ablassmeier <ma@sep.de>
2014-03-06 12:18:53 +0100

    argparse

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ getotrs
 =======
 
 Download otrs ticket attachments to local folder
+
+USAGE
+------------
+
+<pre>
+usage: getotrs.py [-h] --url URL --ticket TICKET --user USER --pw PW
+                  [--folder FOLDER]
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --url URL        Base URL to otrs (http://host/)
+  --ticket TICKET  Ticket URL to download
+                   (otrs/index.pl?Action=AgentTicketZoom;TicketID=7496)
+  --user USER      OTRS Username
+  --pw PW          OTRS Password
+  --folder FOLDER  Folder to download stuff (default full subject ticket id)
+</pre>
+
+EXAMPLE
+------------
+<pre>
+getotrs.py --url https://otrs.url.de/ --ticket 'otrs/index.pl?Action=AgentTicketZoom;TicketID=7496' --user username --pw password
+</pre>
+
--- getotrs.py
+++ getotrs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import argparse
 import mechanize
 import requests
@@ -9,6 +10,7 @@ parser.add_argument("--url", help="Base URL to otrs (http://host/)", type=str, r
 parser.add_argument("--ticket", help="Ticket URL to download (otrs/index.pl?Action=AgentTicketZoom;TicketID=7496)", type=str, required=1)
 parser.add_argument("--user", help="OTRS Username", type=str, required=1)
 parser.add_argument("--pw", help="OTRS Password", type=str, required=1)
+parser.add_argument("--folder", help="Folder to download stuff (default full subject ticket id)", type=str, required=0)
 args = parser.parse_args()
 
 base_url = args.url
@@ -28,7 +30,14 @@ data = BeautifulSoup(browser.response().read())
 
 if 'Login failed!' in data.get_text():
     print 'Login failed! Your username or password was entered incorrectly.'
-    exit
+    exit(1)
+
+if args.folder:
+    target_folder= args.folder
+else:
+    target_folder =  data.title.string.split(' ')[0]
+
+print "Target Folder:" + target_folder
 
 attachments=[]
 for a in data.find_all('a', href=True):
@@ -37,8 +46,11 @@ for a in data.find_all('a', href=True):
         if a['href'] not in attachments:
             attachments.append(a['href'])
 
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
+
 for file in attachments:
     print base_url+file
     t = file.split('?')
     n = t[0].split('/')
-    browser.retrieve(base_url+file, n[3])
+    browser.retrieve(base_url+file, target_folder + '/' + n[3])
