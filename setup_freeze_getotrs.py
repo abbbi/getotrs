f9cd9b47a42f8ee5f01dac13caf17940b7b8e993
ma <ma@sep.de>
2014-06-03 17:07:17 +0200

    cx_freeze script

---
+++ setup_freeze_getotrs.py
@@ -0,0 +1,15 @@
+from cx_Freeze import setup, Executable
+
+buildOptions = dict(
+        compressed = True,
+        append_script_to_exe = False,
+        create_shared_zip = False)
+setup(
+    name = "getotrs",
+    version = "0.1",
+    description = "getotrs",
+    options = dict(build_exe = buildOptions),
+    # targets to build
+    executables = [Executable("getotrs.py")]
+)
+
