a0b7074e9285f0104d01f895f637153da7f46a22
ma <ma@sep.de>
2021-08-24 08:38:09 +0200

    Reformat code using black, use github actions

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -9,23 +10,22 @@ def read(fname):
 from getotrs import __version__
 
 setup(
-    name='getotrs',
+    name="getotrs",
     version=__version__,
-    description='Download otrs ticket attachments',
-    url='https://github.com/abbbi/getotrs/',
-    author='Michael Ablassmeier',
-    author_email='abi@grinser.de',
-    license='GPL',
-    keywords='otrs download attachment',
-    packages=find_packages(exclude=('docs', 'tests', 'env')),
+    description="Download otrs ticket attachments",
+    url="https://github.com/abbbi/getotrs/",
+    author="Michael Ablassmeier",
+    author_email="abi@grinser.de",
+    license="GPL",
+    keywords="otrs download attachment",
+    packages=find_packages(exclude=("docs", "tests", "env")),
     include_package_data=True,
-    install_requires=[
-    ],
-    scripts=['getotrs'],
+    install_requires=[],
+    scripts=["getotrs"],
     extras_require={
-    'dev': [],
-    'docs': [],
-    'testing': [],
+        "dev": [],
+        "docs": [],
+        "testing": [],
     },
     classifiers=[],
-    )
+)
--- setup_freeze_getotrs.py
+++ setup_freeze_getotrs.py
@@ -1,15 +1,13 @@
 from cx_Freeze import setup, Executable
 
 buildOptions = dict(
-        compressed = True,
-        append_script_to_exe = False,
-        create_shared_zip = False)
-setup(
-    name = "getotrs",
-    version = "0.1",
-    description = "getotrs",
-    options = dict(build_exe = buildOptions),
-    # targets to build
-    executables = [Executable("getotrs.py")]
+    compressed=True, append_script_to_exe=False, create_shared_zip=False
 )
-
+setup(
+    name="getotrs",
+    version="0.1",
+    description="getotrs",
+    options=dict(build_exe=buildOptions),
+    # targets to build
+    executables=[Executable("getotrs.py")],
+)
